dc1e0df80261172271a82a21312a9dc911c1f4d6
Maciej Sasinowski <38035075+msasinowski@users.noreply.github.com>
2024-11-27 22:32:32 +0100

    fix: promise reject for paypal failure (#28)
    Co-authored-by: Maciej Sasinowski <maciej.sasinowski2@frontdoor.com>

--- android/src/main/java/com/expobraintree/ExpoBraintreeModuleHandlers.kt
+++ android/src/main/java/com/expobraintree/ExpoBraintreeModuleHandlers.kt
@@ -33,7 +33,19 @@ class PaypalRebornModuleHandlers {
         ))
       return
     }
-    mPromise.reject(error.message)
+    error.message?.let {
+      mPromise.reject(EXCEPTION_TYPES.KOTLIN_EXCEPTION.value,
+        ERROR_TYPES.TOKENIZE_VAULT_PAYMENT_ERROR.value,
+        PaypalDataConverter.createError(
+          EXCEPTION_TYPES.KOTLIN_EXCEPTION.value, error.message
+        ))
+    } ?: {
+      mPromise.reject(EXCEPTION_TYPES.KOTLIN_EXCEPTION.value,
+        ERROR_TYPES.TOKENIZE_VAULT_PAYMENT_ERROR.value,
+        PaypalDataConverter.createError(
+          EXCEPTION_TYPES.KOTLIN_EXCEPTION.value, "PayPal Error"
+        ))
+    }
   }
 
   public fun onPayPalSuccessHandler(payPalAccountNonce: PayPalAccountNonce, mPromise: Promise) {
